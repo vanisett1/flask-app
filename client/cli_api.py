475cfa29f5d8e6dcaf76c2c2c190273935c3d142
vanisetti1 <vanisetti1@gmail.com>
2023-11-23 05:15:19 -0500

    edokati

--- client/cli_api.py
+++ client/cli_api.py
@@ -1,7 +1,7 @@
 import inquirer
 import subprocess
 
-BASE_URL = "http://localhost:5000"  # Base URL for the API
+BASE_URL = "https://flask-app.yellowcoast-6fa59d16.eastus.azurecontainerapps.io"  # Base URL for the API
 
 def run_curl_command(curl_cmd):
     try:
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', PORT = '5000', debug=True)
+    app.run(debug=True)
 
